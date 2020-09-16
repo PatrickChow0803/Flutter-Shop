5b5ea21029fdc99e4526bc262ec905b28de5e33f
patrickchow0803 <patrickchow0803@gmail.com>
2020-09-16 10:44:35 -0400

    Made orders expandable

--- lib/widgets/order_item.dart
+++ lib/widgets/order_item.dart
@@ -1,11 +1,19 @@
 import 'package:flutter/material.dart';
 import '../provider/orders.dart' as ord;
 import 'package:intl/intl.dart';
+import 'dart:math';
 
-class OrderItem extends StatelessWidget {
+class OrderItem extends StatefulWidget {
   final ord.OrderItem order;
 
   OrderItem(this.order);
+
+  @override
+  _OrderItemState createState() => _OrderItemState();
+}
+
+class _OrderItemState extends State<OrderItem> {
+  var _expanded = false;
 
   @override
   Widget build(BuildContext context) {
@@ -14,15 +22,45 @@ class OrderItem extends StatelessWidget {
       child: Column(
         children: [
           ListTile(
-            title: Text('\$${order.amount}'),
+            title: Text('\$${widget.order.amount}'),
             subtitle: Text(
-              DateFormat('dd/MM/yyyy hh:mm').format(order.dateTime),
+              DateFormat('dd/MM/yyyy hh:mm').format(widget.order.dateTime),
             ),
             trailing: IconButton(
-              icon: Icon(Icons.expand_more),
-              onPressed: () {},
+              icon: Icon(_expanded ? Icons.expand_less : Icons.expand_more),
+              onPressed: () {
+                setState(() {
+                  _expanded = !_expanded;
+                });
+              },
             ),
-          )
+          ),
+          // If _expanded is true, show this container widget.
+          if (_expanded)
+            Container(
+              padding: EdgeInsets.symmetric(horizontal: 15, vertical: 4),
+              // set the height of the container based off of how many products are in the order
+              // But take the lower of the two numbers.
+              height: min(widget.order.products.length * 20.0 + 10.0, 100),
+              child: ListView(
+                // Convert a list of data into a list of widgets
+                children: widget.order.products
+                    .map((prod) => Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Text(
+                              prod.title,
+                              style: TextStyle(fontSize: 18.0, fontWeight: FontWeight.bold),
+                            ),
+                            Text(
+                              '${prod.quantity} x \$${prod.price}',
+                              style: TextStyle(fontSize: 18.0, color: Colors.grey),
+                            ),
+                          ],
+                        ))
+                    .toList(),
+              ),
+            )
         ],
       ),
     );
