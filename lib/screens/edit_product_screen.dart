c03cb559a2343f45703482cb87950e8bd89a1dac
patrickchow0803 <patrickchow0803@gmail.com>
2020-09-17 10:11:21 -0400

    Added a basic edit product screen

---
+++ lib/screens/edit_product_screen.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+// Stateful here because have to mange user input
+class EditProductScreen extends StatefulWidget {
+  @override
+  _EditProductScreenState createState() => _EditProductScreenState();
+}
+
+class _EditProductScreenState extends State<EditProductScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Edit Product'),
+      ),
+      body: ,
+    );
+  }
+}
