949f979acad19590359a5454ea5164e890712938
patrickchow0803 <patrickchow0803@gmail.com>
2020-09-18 14:02:47 -0400

    Added submitting form functionality.

--- lib/screens/edit_product_screen.dart
+++ lib/screens/edit_product_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import '../provider/product.dart';
 
 // Stateful here because have to mange user input
 class EditProductScreen extends StatefulWidget {
@@ -13,6 +14,13 @@ class _EditProductScreenState extends State<EditProductScreen> {
   final _descriptionFocusNode = FocusNode();
   final _imageUrlController = TextEditingController();
   final _imageUrlFocusNode = FocusNode();
+
+  // GlobalKey is used to interact with a widget from inside the code
+  // This variable will be used to save/submit the form
+  final _form = GlobalKey<FormState>();
+
+  // This is the product that the user is trying to edit
+  var _editedProduct = Product(id: null, title: '', price: 0, description: '', imageUrl: '');
 
   @override
   void initState() {
@@ -41,15 +49,34 @@ class _EditProductScreenState extends State<EditProductScreen> {
     super.dispose();
   }
 
+  void _saveForm() {
+    // This is a method provided by the form widget that will save the form
+    // This triggers a method on every TextFormField that will take all the value entered
+    // into a global map.
+    _form.currentState.save();
+
+    print(_editedProduct.title);
+    print(_editedProduct.description);
+    print(_editedProduct.price);
+    print(_editedProduct.imageUrl);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Edit Product'),
+        actions: [
+          IconButton(
+            icon: Icon(Icons.save),
+            onPressed: _saveForm,
+          )
+        ],
       ),
       body: Padding(
         padding: EdgeInsets.all(16.0),
         child: Form(
+          key: _form,
           child: ListView(
             children: [
               TextFormField(
@@ -59,6 +86,17 @@ class _EditProductScreenState extends State<EditProductScreen> {
                 // This will be called whenever the textInputAction is pressed
                 onFieldSubmitted: (value) {
                   FocusScope.of(context).requestFocus(_priceFocusNode);
+                },
+
+                // Creating a new Product each time onSaved is called since all the variables of a product are final
+                onSaved: (value) {
+                  _editedProduct = Product(
+                    title: value,
+                    price: _editedProduct.price,
+                    description: _editedProduct.description,
+                    imageUrl: _editedProduct.imageUrl,
+                    id: null,
+                  );
                 },
               ),
               TextFormField(
@@ -71,13 +109,33 @@ class _EditProductScreenState extends State<EditProductScreen> {
                 onFieldSubmitted: (value) {
                   FocusScope.of(context).requestFocus(_descriptionFocusNode);
                 },
+                // Creating a new Product each time onSaved is called since all the variables of a product are final
+                onSaved: (value) {
+                  _editedProduct = Product(
+                    title: _editedProduct.title,
+                    price: double.parse(value),
+                    description: _editedProduct.description,
+                    imageUrl: _editedProduct.imageUrl,
+                    id: null,
+                  );
+                },
               ),
               TextFormField(
                 decoration: InputDecoration(labelText: 'Description'),
                 maxLines: 3,
                 focusNode: _descriptionFocusNode,
                 keyboardType: TextInputType.multiline,
-                // This will be called whenever the textInputAction is pressed
+
+                // Creating a new Product each time onSaved is called since all the variables of a product are final
+                onSaved: (value) {
+                  _editedProduct = Product(
+                    title: _editedProduct.title,
+                    price: _editedProduct.price,
+                    description: value,
+                    imageUrl: _editedProduct.imageUrl,
+                    id: null,
+                  );
+                },
               ),
               Row(
                 crossAxisAlignment: CrossAxisAlignment.end,
@@ -109,6 +167,20 @@ class _EditProductScreenState extends State<EditProductScreen> {
                       // That's why textInputAction: is set to TextInputAction.done
                       controller: _imageUrlController,
                       focusNode: _imageUrlFocusNode,
+                      // Creating a new Product each time onSaved is called since all the variables of a product are final
+                      onSaved: (value) {
+                        _editedProduct = Product(
+                          title: _editedProduct.title,
+                          price: _editedProduct.price,
+                          description: _editedProduct.description,
+                          imageUrl: value,
+                          id: null,
+                        );
+                      },
+
+                      onFieldSubmitted: (value) {
+                        _saveForm();
+                      },
                     ),
                   ),
                 ],
