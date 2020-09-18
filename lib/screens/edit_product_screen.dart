eb9d26e2a1b930f16ef3c5f9adc69b4c515b1e67
patrickchow0803 <patrickchow0803@gmail.com>
2020-09-18 10:15:58 -0400

    Added Image Input and Image Preview

--- lib/screens/edit_product_screen.dart
+++ lib/screens/edit_product_screen.dart
@@ -11,12 +11,33 @@ class EditProductScreen extends StatefulWidget {
 class _EditProductScreenState extends State<EditProductScreen> {
   final _priceFocusNode = FocusNode();
   final _descriptionFocusNode = FocusNode();
+  final _imageUrlController = TextEditingController();
+  final _imageUrlFocusNode = FocusNode();
+
+  @override
+  void initState() {
+    // Tells Flutter to execute _updateImageUrl whenever focus changes
+    _imageUrlFocusNode.addListener(_updateImageUrl);
+    super.initState();
+  }
+
+  // Instead of always wanting the user to click on the done button for the image for the image to
+  // appear, the user can now just get out of focus and the image will appear.
+  void _updateImageUrl() {
+    if (!_imageUrlFocusNode.hasFocus) {
+      // This causes the screen to rebuild
+      setState(() {});
+    }
+  }
 
   // Have to get rid of the focus nodes otherwise they'll just stay here and cause a memory leak.
   @override
   void dispose() {
+    _imageUrlFocusNode.removeListener(_updateImageUrl);
     _priceFocusNode.dispose();
     _descriptionFocusNode.dispose();
+    _imageUrlController.dispose();
+    _imageUrlFocusNode.dispose();
     super.dispose();
   }
 
@@ -58,6 +79,40 @@ class _EditProductScreenState extends State<EditProductScreen> {
                 keyboardType: TextInputType.multiline,
                 // This will be called whenever the textInputAction is pressed
               ),
+              Row(
+                crossAxisAlignment: CrossAxisAlignment.end,
+                children: [
+                  Container(
+                    width: 100,
+                    height: 100,
+                    margin: EdgeInsets.only(
+                      top: 8,
+                      right: 10,
+                    ),
+                    decoration: BoxDecoration(
+                      border: Border.all(width: 1, color: Colors.grey),
+                    ),
+                    child: _imageUrlController.text.isEmpty
+                        ? Text('Enter a URL')
+                        : FittedBox(
+                            child: Image.network(_imageUrlController.text),
+                            fit: BoxFit.cover,
+                          ),
+                  ),
+                  Expanded(
+                    child: TextFormField(
+                      decoration: InputDecoration(labelText: 'Image Url'),
+                      keyboardType: TextInputType.url,
+                      textInputAction: TextInputAction.done,
+                      // Have this controller because I want this information before the form is submitted.
+                      // This controller is updated only when the done button is pressed.
+                      // That's why textInputAction: is set to TextInputAction.done
+                      controller: _imageUrlController,
+                      focusNode: _imageUrlFocusNode,
+                    ),
+                  ),
+                ],
+              )
             ],
           ),
         ),
