c51fc384b60390a9dd8ac2bdba124e608906f38e
patrickchow0803 <patrickchow0803@gmail.com>
2020-09-18 09:55:32 -0400

    Added a form text field for the description of a product

--- lib/screens/edit_product_screen.dart
+++ lib/screens/edit_product_screen.dart
@@ -10,6 +10,15 @@ class EditProductScreen extends StatefulWidget {
 
 class _EditProductScreenState extends State<EditProductScreen> {
   final _priceFocusNode = FocusNode();
+  final _descriptionFocusNode = FocusNode();
+
+  // Have to get rid of the focus nodes otherwise they'll just stay here and cause a memory leak.
+  @override
+  void dispose() {
+    _priceFocusNode.dispose();
+    _descriptionFocusNode.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -38,6 +47,16 @@ class _EditProductScreenState extends State<EditProductScreen> {
                 // Makes it so that only numbers can be entered in the soft keyboard
                 keyboardType: TextInputType.number,
                 focusNode: _priceFocusNode,
+                onFieldSubmitted: (value) {
+                  FocusScope.of(context).requestFocus(_descriptionFocusNode);
+                },
+              ),
+              TextFormField(
+                decoration: InputDecoration(labelText: 'Description'),
+                maxLines: 3,
+                focusNode: _descriptionFocusNode,
+                keyboardType: TextInputType.multiline,
+                // This will be called whenever the textInputAction is pressed
               ),
             ],
           ),
